304518620730832a6e1b11ad55493d6ac5c9a70a
stella-pliatsiou <72984337+stella-pliatsiou@users.noreply.github.com>
2020-12-09 22:57:59 +0200

    python for everybody - coursera

---
+++ ask2.py
@@ -0,0 +1,16 @@
+numlist = list()
+
+while True :
+
+    inp = input('Enter a number: ')
+
+    if inp == 'done' : break
+    value = float(inp)
+
+    numlist.append(value)
+
+​
+
+average = sum(numlist) / len(numlist)
+
+print('Average:', average)
